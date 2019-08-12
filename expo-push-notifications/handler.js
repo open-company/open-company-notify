0ded9bff553b88c62b1165c0ac75809233b51c2c
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-08-12 12:42:11 -0400

    Add getPushNotificationReceipts Lambda function to the serverless project

--- expo-push-notifications/handler.js
+++ expo-push-notifications/handler.js
@@ -57,3 +57,73 @@ module.exports.sendPushNotifications = async event => {
     ),
   };
 };
+
+// Later, after the Expo push notification service has delivered the
+// notifications to Apple or Google (usually quickly, but allow the the service
+// up to 30 minutes when under load), a "receipt" for each notification is
+// created. The receipts will be available for at least a day; stale receipts
+// are deleted.
+//
+// The ID of each receipt is sent back in the response "ticket" for each
+// notification. In summary, sending a notification produces a ticket, which
+// contains a receipt ID you later use to get the receipt.
+//
+// The receipts may contain error codes to which you must respond. In
+// particular, Apple or Google may block apps that continue to send
+// notifications to devices that have blocked notifications or have uninstalled
+// your app. Expo does not control this policy and sends back the feedback from
+// Apple and Google so you can handle it appropriately.
+
+module.exports.getPushNotificationReceipts = async event => {
+
+  const tickets = event.tickets;
+  let expo = new Expo();
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    // NOTE: Not all tickets have IDs; for example, tickets for notifications
+    // that could not be enqueued will have error information and no receipt ID.
+    if (ticket.id) {
+      receiptIds.push(ticket.id);
+    }
+  }
+
+  let receipts = [];
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+  for (let chunk of receiptIdChunks) {
+    try {
+      let receiptChunk = await expo.getPushNotificationReceiptsAsync(chunk);
+      console.log(receiptChunk);
+      receipts.push(...receiptChunk);
+
+      // The receipts specify whether Apple or Google successfully received the
+      // notification and information about an error, if one occurred.
+      for (let receipt of receiptChunk) {
+        if (receipt.status === 'ok') {
+          continue;
+        } else if (receipt.status === 'error') {
+          // Do some error logging for audit purposes
+          console.error(`There was an error sending a notification: ${receipt.message}`);
+          if (receipt.details && receipt.details.error) {
+            console.error(
+              `The error code is ${receipt.details.error}. See https://docs.expo.io/versions/latest/guides/push-notifications#response-format for error code docs.`
+            );
+          }
+        }
+      }
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            receipts: receipts
+          },
+          null,
+          2
+        )
+      };
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
