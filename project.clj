4d9fa60cef4a9424f626bc01d6b473aa4cb9f1a6
bago2k4 <iacopo@rc0.eu>
2020-11-19 18:25:46 +0100

    Update lib version.

--- project.clj
+++ project.clj
@@ -43,7 +43,13 @@
     [com.fasterxml.jackson.core/jackson-databind "2.11.2"]
 
     ;; Library for OC projects https://github.com/open-company/open-company-lib
-    [open-company/lib "0.17.29-alpha45"]
+    ;; ************************************************************************
+    ;; ****************** NB: don't go under 0.17.29-alpha56 ******************
+    ;; ***************** (JWT schema changes, more info here: *****************
+    ;; ******* https://github.com/open-company/open-company-lib/pull/82) ******
+    ;; ************************************************************************
+    [open-company/lib "0.17.29-alpha56" :exclusions [commons-codec]]
+    ;; ************************************************************************
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; httpkit - Web server http://http-kit.org/
     ;; core.async - Async programming and communication https://github.com/clojure/core.async
