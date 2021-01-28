983eafc89141f373f52b31179ba0a9ce09dce875
bago2k4 <iacopo@rc0.eu>
2021-01-28 19:31:01 +0100

    Update lib and remove explicit sentry appender.

--- src/oc/notify/app.clj
+++ src/oc/notify/app.clj
@@ -384,11 +384,7 @@
   [port]
 
   ;; Stuff logged at error level goes to Sentry
-  (if c/dsn
-    (timbre/merge-config!
-      {:level (keyword c/log-level)
-       :appenders {:sentry (sentry/sentry-appender c/sentry-config)}})
-    (timbre/merge-config! {:level (keyword c/log-level)}))
+  (timbre/merge-config! {:level (keyword c/log-level)})
 
   ;; Start the system
   (-> {:httpkit {:handler-fn app :port port}
