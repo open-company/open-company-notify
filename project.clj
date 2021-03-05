58d37e47e5f7a0b8be55aceb952aa7490102a009
bago2k4 <iacopo@rc0.eu>
2021-03-05 13:59:39 +0100

    Bump lib.

--- project.clj
+++ project.clj
@@ -50,7 +50,7 @@
     ;; ***************** (JWT schema changes, more info here: *****************
     ;; ******* https://github.com/open-company/open-company-lib/pull/82) ******
     ;; ************************************************************************
-    [open-company/lib "0.17.34-alpha8" :exclusions [commons-codec org.clojure/tools.reader]]
+    [open-company/lib "0.17.34-alpha9" :exclusions [commons-codec org.clojure/tools.reader]]
     ;; ************************************************************************
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; httpkit - Web server http://http-kit.org/
